5f9b313fd86f4ca5e75cab6c0400931d9ee12726
Martin <MartinDitlefsen@gmail.com>
2021-08-17 22:07:12 +0200

    Pre-optimization Oppgave 2B

--- del1/js/oppg2a.js
+++ del1/js/oppg2a.js
@@ -52,7 +52,7 @@ function initVertexBuffers(gl) {
     let vertices = new Float32Array([
         0.7, 0.5, 0,
         -0.5, -0.9, 0,
-        0.5, -0.5, 0,
+        0.5, -0.75, 0,
         -0.3, 0.6, 0,
         -0.1, -0.9, 0,
         0.5, -0.8, 0,
---
+++ del1/js/oppg2b.js
@@ -0,0 +1,273 @@
+
+let VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute vec4 a_Color;
+    varying vec4 v_Color;
+    void main() {
+        gl_Position = a_Position;
+        v_Color = a_Color;
+    }`;
+
+let FSHADER_SOURCE = `
+    precision mediump float;
+    varying vec4 v_Color;
+    void main() {
+        gl_FragColor = v_Color;
+    }`;
+
+function main(){
+
+    let canvas = document.getElementById('webgl');
+
+    let gl = canvas.getContext('webgl');
+    if(!gl){
+        console.log("Klarte ikke å hente ut webgl konteksten");
+        return;
+    }
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+        console.log("Klarte ikke å initialisere shaders");
+        return;
+    }
+
+
+    gl.clearColor(0.95, 0.95, 0.95, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let numAxes = createAxes(gl);
+    if(numAxes <= 0){
+        return;
+    }
+    let verticesPerLine = 2;
+
+    gl.drawArrays(gl.LINES, 0, numAxes * verticesPerLine);
+
+    let numTris = createAxisTriangles(gl);
+    if(numTris <= 0){
+        return;
+    }
+    let verticesPerTri = 3;
+    gl.drawArrays(gl.TRIANGLES, 0, numAxes * verticesPerTri);
+
+    numTris = createBigArrow(gl);
+    if(numTris <= 0){
+        return;
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, numAxes * verticesPerTri);
+
+}
+
+function createAxes(gl){
+    let vertices = new Float32Array([
+        0.8, 0.0, 0,
+        -0.8, 0.0, 0,
+        0.0, 0.8, 0,
+        0.0, -0.8, 0
+    ]);
+
+    let colors = new Float32Array([
+        0.9, 0.0, 0.0, 1.0,
+        0.9, 0.0, 0.0, 1.0,
+        0.0, 0.7, 0.3, 1.0,
+        0.0, 0.7, 0.3, 1.0
+    ]);
+
+    let colBuffer = gl.createBuffer();
+    if(!colBuffer){
+        console.log("Klarte ikke å lage en buffer");
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    let colAttrib = gl.getAttribLocation(gl.program, 'a_Color');
+    if(colAttrib < 0){
+        console.log("Klarte ikke å finne farge attributten: 'a_Color'");
+        return -1;
+    }
+
+    let floatsPerColor = 4;
+    gl.vertexAttribPointer(colAttrib, floatsPerColor, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colAttrib);
+
+    let vertBuffer = gl.createBuffer();
+    if(!vertBuffer){
+        console.log("Klarte ikke å lage en buffer");
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    let vertAttrib = gl.getAttribLocation(gl.program, 'a_Position');
+    if(vertAttrib < 0){
+        console.log("Klarte ikke å finne posisjons attributten: 'a_Position'");
+        return -1;
+    }
+
+    let floatsPerVertex = 3;
+    gl.vertexAttribPointer(vertAttrib, floatsPerVertex, gl.FLOAT, false, 0, 0);
+
+    gl.enableVertexAttribArray(vertAttrib);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    return vertices.length / 2;
+}
+
+function createAxisTriangles(gl){
+    let vertices = new Float32Array([
+        0.8, 0.025, 0,
+        0.8, -0.025, 0,
+        0.84, 0.0, 0,
+        -0.8, 0.025, 0,
+        -0.8, -0.025, 0,
+        -0.84, 0.0, 0,
+        0.025, 0.8, 0,
+        -0.025, 0.8, 0,
+        0.0, 0.84, 0,
+        0.025, -0.8, 0,
+        -0.025, -0.8, 0,
+        0.0, -0.84, 0
+    ]);
+
+    let colors = new Float32Array([
+        0.9, 0.0, 0.0, 1.0,
+        0.9, 0.0, 0.0, 1.0,
+        0.9, 0.0, 0.0, 1.0,
+        0.9, 0.0, 0.0, 1.0,
+        0.9, 0.0, 0.0, 1.0,
+        0.9, 0.0, 0.0, 1.0,
+        0.0, 0.7, 0.3, 1.0,
+        0.0, 0.7, 0.3, 1.0,
+        0.0, 0.7, 0.3, 1.0,
+        0.0, 0.7, 0.3, 1.0,
+        0.0, 0.7, 0.3, 1.0,
+        0.0, 0.7, 0.3, 1.0
+    ]);
+
+    let colBuffer = gl.createBuffer();
+    if(!colBuffer){
+        console.log("Klarte ikke å lage en buffer");
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    let colAttrib = gl.getAttribLocation(gl.program, 'a_Color');
+    if(colAttrib < 0){
+        console.log("Klarte ikke å finne farge attributten: 'a_Color'");
+        return -1;
+    }
+
+    let floatsPerColor = 4;
+    gl.vertexAttribPointer(colAttrib, floatsPerColor, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colAttrib);
+
+    let vertBuffer = gl.createBuffer();
+    if(!vertBuffer){
+        console.log("Klarte ikke å lage en buffer");
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    let vertAttrib = gl.getAttribLocation(gl.program, 'a_Position');
+    if(vertAttrib < 0){
+        console.log("Klarte ikke å finne posisjons attributten: 'a_Position'");
+        return -1;
+    }
+
+    let floatsPerVertex = 3;
+    gl.vertexAttribPointer(vertAttrib, floatsPerVertex, gl.FLOAT, false, 0, 0);
+
+    gl.enableVertexAttribArray(vertAttrib);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    return vertices.length / 3;
+}
+
+function createBigArrow(gl){
+    let vertices = new Float32Array([
+        -0.7,  0.25, 1.0,
+        -0.45, 0.00, 1.0,
+        0.2,   0.25, 1.0,
+        -0.7, -0.25, 1.0,
+        -0.45, 0.00, 1.0,
+        0.2,  -0.25, 1.0,
+        -0.45, 0.00, 1.0,
+        0.2,  -0.25, 1.0,
+        0.2,   0.25, 1.0,
+        0.2,   0.50, 1.0,
+        0.2,  -0.50, 1.0,
+        0.7,   0.00, 1.0
+    ]);
+
+    let colors = new Float32Array([
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85,
+        0.0, 0.59, 0.84, 0.85
+    ]);
+
+    let colBuffer = gl.createBuffer();
+    if(!colBuffer){
+        console.log("Klarte ikke å lage en buffer");
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    let colAttrib = gl.getAttribLocation(gl.program, 'a_Color');
+    if(colAttrib < 0){
+        console.log("Klarte ikke å finne farge attributten: 'a_Color'");
+        return -1;
+    }
+
+    let floatsPerColor = 4;
+    gl.vertexAttribPointer(colAttrib, floatsPerColor, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colAttrib);
+
+    let vertBuffer = gl.createBuffer();
+    if(!vertBuffer){
+        console.log("Klarte ikke å lage en buffer");
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    let vertAttrib = gl.getAttribLocation(gl.program, 'a_Position');
+    if(vertAttrib < 0){
+        console.log("Klarte ikke å finne posisjons attributten: 'a_Position'");
+        return -1;
+    }
+
+    let floatsPerVertex = 3;
+    gl.vertexAttribPointer(vertAttrib, floatsPerVertex, gl.FLOAT, false, 0, 0);
+
+    gl.enableVertexAttribArray(vertAttrib);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    return vertices.length / 3;
+}
