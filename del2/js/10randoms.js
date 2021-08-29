b47f0bf9ec92af2b8f4458ff07290fe25cb73270
Martin <MartinDitlefsen@gmail.com>
2021-08-29 15:37:50 +0200

    oppg 2 modul2

--- del2/js/10randoms.js
+++ del2/js/10randoms.js
@@ -1,8 +1,10 @@
 let VSHADER_SOURCE = `
    attribute vec4 a_Position;
+   uniform mat4 u_modelviewMatrix;
+   uniform mat4 u_projectionMatrix;
    attribute float a_PointSize;
    void main() {
-     gl_Position = a_Position;
+     gl_Position = u_projectionMatrix * u_modelviewMatrix * a_Position;
      gl_PointSize = a_PointSize;
    }`;
 
@@ -14,6 +16,13 @@ let FSHADER_SOURCE = `
     }`;
 
 function main(){
+    let u_modelviewMatrix = null;
+    let u_projectionMatrix = null;
+    let modelMatrix = new Matrix4();
+    let viewMatrix = new Matrix4();
+    let modelviewMatrix = new Matrix4();
+    let projectionMatrix = new Matrix4();
+
     let canvas = document.getElementById('webgl');
 
     let gl = canvas.getContext('webgl');
@@ -42,6 +51,24 @@ function main(){
         return console.log('Fant ikke uniform-parametret u_FragColor i shaderen!?');
     }
 
+    u_modelviewMatrix = gl.getUniformLocation(gl.program, 'u_modelviewMatrix');
+    u_projectionMatrix = gl.getUniformLocation(gl.program, 'u_projectionMatrix');
+
+    modelMatrix.setIdentity();
+
+    let  eyeX=0, eyeY=70, eyeZ=100;
+    let  lookX=0, lookY=0, lookZ=0;
+    let  upX=0, upY=1, upZ=0;
+    viewMatrix.setLookAt(eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+
+    modelMatrix.setTranslate(0, 15, 0);
+    modelMatrix.scale(30, 30, 30);
+    modelviewMatrix = viewMatrix.multiply(modelMatrix);
+    projectionMatrix.setPerspective(45, canvas.width / canvas.height, 1, 1000);
+
+    gl.uniformMatrix4fv(u_modelviewMatrix, false, modelviewMatrix.elements);
+    gl.uniformMatrix4fv(u_projectionMatrix, false, projectionMatrix.elements);
+
     gl.vertexAttrib1f(a_PointSize, 4.0);
     gl.uniform4f(u_FragColor, 1.0, 1.0, 0.0, 1.0);
 
@@ -54,6 +81,12 @@ function main(){
         gl.drawArrays(gl.POINTS, 0, 1);
     }
 
+    viewMatrix.setLookAt(eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+    modelMatrix.setIdentity();
+    modelMatrix.setTranslate(15, 15, 0);
+    modelMatrix.scale(35, 35, 35);
+    modelviewMatrix = viewMatrix.multiply(modelMatrix);
+    gl.uniformMatrix4fv(u_modelviewMatrix, false, modelviewMatrix.elements);
     let vertices = new Float32Array([
         -0.5, -0.7, 1.0,
         -0.3, -0.6, 1.0,
@@ -76,5 +109,22 @@ function main(){
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 5);
+
+    viewMatrix.setLookAt(eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+    modelMatrix.setIdentity();
+    modelMatrix.setTranslate(-25, 15, 0);
+    modelMatrix.scale(25, 25, 25);
+    modelviewMatrix = viewMatrix.multiply(modelMatrix);
+    gl.uniformMatrix4fv(u_modelviewMatrix, false, modelviewMatrix.elements);
+    gl.drawArrays(gl.LINE_STRIP, 0, 4);
+
+    viewMatrix.setLookAt(eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+    modelMatrix.setIdentity();
+    modelMatrix.setTranslate(-15, 40, 0);
+    modelMatrix.scale(25, 25, 25);
+    modelviewMatrix = viewMatrix.multiply(modelMatrix);
+    gl.uniformMatrix4fv(u_modelviewMatrix, false, modelviewMatrix.elements);
+    gl.drawArrays(gl.LINES, 0, 4);
+
 
 }
